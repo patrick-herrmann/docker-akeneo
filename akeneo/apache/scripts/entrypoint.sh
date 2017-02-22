55c3de0e8b3a13387e28b715aead8e3c71666926
Patrick Herrmann <patrick@mbp-053.local>
2017-02-22 17:33:02 +0100

    No MySQL safe check while running the script, because the host can not be resolved

--- akeneo/apache/scripts/entrypoint.sh
+++ akeneo/apache/scripts/entrypoint.sh
@@ -72,21 +72,21 @@ if [ "$DO_INIT_AKENEO" = "yes" ]; then
 
 	# Attente MySQL
 	TERM=dumb php -- "$MYSQL_LINK" "$MYSQL_USER" "$MYSQL_PASSWORD" <<'EOPHP'
-<?php
-$stderr = fopen('php://stderr', 'w');
-$maxTries = 10;
-do {
-	$mysql = new mysqli($argv[1], $argv[2], $argv[3]);
-	if ($mysql->connect_error) {
-		fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
-		--$maxTries;
-		if ($maxTries <= 0) {
-			exit(1);
-		}
-		sleep(15);
-	}
-} while ($mysql->connect_error);
-EOPHP
+# <?php
+# $stderr = fopen('php://stderr', 'w');
+# $maxTries = 10;
+# do {
+# 	$mysql = new mysqli($argv[1], $argv[2], $argv[3]);
+# 	if ($mysql->connect_error) {
+# 		fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+# 		--$maxTries;
+# 		if ($maxTries <= 0) {
+# 			exit(1);
+# 		}
+# 		sleep(15);
+# 	}
+# } while ($mysql->connect_error);
+# EOPHP
 	echo 'MySQL OK ...'
 
 	# Attente MongoDB
